6cc18b76897953ff0f6e536d380505e2ef2b7a86
shakerchinthakuntla <shaker.chinthakuntla@gmail.com>
2024-01-19 17:04:39 +0530

    Learn Spring FW

---
+++ src/main/java/com/cspstech/LearnSpringFwApplication.java
@@ -0,0 +1,13 @@
+package com.cspstech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringFwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringFwApplication.class, args);
+	}
+
+}
